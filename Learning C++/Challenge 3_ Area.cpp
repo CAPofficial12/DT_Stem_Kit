5758fbd1a004d509bae8e11642efe3490412f183
Aarav <athakur@dohacollege.com.qa>
2026-04-07 20:50:59 +0300

    Challenge 3

---
+++ Learning C++/Challenge 3_ Area.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(){
+
+    int length;
+    length = 5;
+
+    int width;
+    width = 7;
+
+    int area;
+    area = {length * width};
+
+    std::cout << area << "\n";
+
+    return 0;
+}
