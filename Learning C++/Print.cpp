6a4acea2a993cae9a0e3ad61852c08aad68d0cd2
Aarav <athakur@dohacollege.com.qa>
2026-04-07 16:20:37 +0300

    Learnt how to assign values to variables

--- Learning C++/Print.cpp
+++ Learning C++/Print.cpp
@@ -7,5 +7,12 @@ int main()
     std::cout << a;
     std::cout << "Hello World" << std::endl;
     std::cout << "The endl moves the print function to the next line" << std::endl;
+
+    int value {100};
+    std::cout << value << std::endl;
+
+    int value2 (100.7);
+    std::cout << value2 << std::endl;
+
     return 0;
 }
