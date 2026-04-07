44e35396f03b3f652f1500ec942ef5d074371d6b
Aarav <athakur@dohacollege.com.qa>
2026-04-07 16:17:44 +0300

    Learn Print

---
+++ Learning C++/Print.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+int main()
+{
+    int a =5;
+
+    std::cout << a;
+    std::cout << "Hello World" << std::endl;
+    std::cout << "The endl moves the print function to the next line" << std::endl;
+    return 0;
+}
