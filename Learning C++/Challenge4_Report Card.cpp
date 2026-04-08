8150043d459eac2cc9aaf06e06fe925588a4e240
Aarav <athakur@dohacollege.com.qa>
2026-04-08 06:03:09 +0300

    Data type usage

---
+++ Learning C++/Challenge4_Report Card.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main (){
+    int student_ID;
+    student_ID = 50.6;
+
+    float score;
+    score = 50.78;
+
+    char grade;
+    grade = 'A';
+
+    std::cout << student_ID << std::endl;
+    std::cout << score << "\n";
+    std::cout << grade;
+
+    return 0;
+}
