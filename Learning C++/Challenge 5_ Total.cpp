f58a697a875399fec50a49ed1917952005f834b7
Aarav <athakur@dohacollege.com.qa>
2026-04-08 06:17:57 +0300

    Challenge 5 completed

---
+++ Learning C++/Challenge 5_ Total.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(){
+
+    int itemPrice;
+    itemPrice = 50;
+
+    int shippingCost;
+    shippingCost = 50;
+
+    int sum;
+    sum = itemPrice + shippingCost;
+
+    std::cout << sum << "\n";
+    return 0;
+}
